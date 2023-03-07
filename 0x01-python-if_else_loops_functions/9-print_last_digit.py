1dfc7fead89ff2358af7cc6fc3db4953c1fdee10
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-03-07 14:56:20 -0800

    last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_las_digit_(number):
-    print(abs(number) % 10, end="")
-    return(abs(number) % 10)
+     if number < 0:
+        number = number * -1
+    r = number % 10
+    print('{}'.format(r), end="")
+    return r
