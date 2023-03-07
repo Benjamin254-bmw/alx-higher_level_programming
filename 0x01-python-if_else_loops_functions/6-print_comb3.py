80a40d717e5b696254837975d17b062eceeeb178
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-03-07 14:07:56 -0800

    combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for number1 in range(0, 100):
     for number2 in range(number1 + 1, 10):
-        if (number1 == 8 and number2 == 9):
+        if number1 == 8 and number2 == 9:
+            print("{}{}".format(number1, number2))
+        else:
             print("{}{}".format(number1, number2), end=", ")
