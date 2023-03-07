8990095131920c360f63ffa904dfdd77020aedb3
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-03-07 12:54:54 -0800

    alphabets

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
     if char(letter) != 'q' and char(letter)!= 'e':
-        print('{}'.format(char(letter)) end=""):
+        print('{}'.format(char(letter)), end=""):
