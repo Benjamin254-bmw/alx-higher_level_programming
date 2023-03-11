1bba7423d7625e521ea04cfc591e26e6d6061ea3
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-03-11 03:23:00 -0800

    format

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,22 +5,8 @@ a = 10
 b = 5
 
 if __name__ == "__main__":
-    def add(a, b):
-        add(1 + 2)
         print("{} + {} = {}".format(a, b, add(a, b)))
-        return(a + b)
-
-    def sub(a, b):
-        sub(a, b)
         print("{} - {} = {}".format(a, b, sub(a, b)))
-        return (a - b)
-
-    def mul(a, b):
-        mul(a, b)
         print("{} * {} = {}".format(a, b, mul(a, b)))
-        return (a * b)
-
-    def div(a, b):
-        div(a, b)
         print("{} / {} = {}".format(a, b, div(a, b)))
-        return int(a / b)
+        
