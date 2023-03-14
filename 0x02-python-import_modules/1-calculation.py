a748ce3b58dfa91a3d8a3ba0ad349d43a375e5bf
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-03-15 02:00:44 +0300

    calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-from claculator_1 import add, sub, mul, div
-
-a = 10
-b = 5
-
 if __name__ == "__main__":
+
+    from claculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
