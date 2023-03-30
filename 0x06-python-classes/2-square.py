e51bbf3b815f1c0c31020df7b556dfe73bfaac06
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-03-29 19:03:52 -0700

    defines a square by: (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square:
+    """create a class named Square
+    """
+
+    def __init__(self, size=0):
+        """__init__ method initializes an instance
+        Arg:
+            size: private instance attribute
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be greater or equal than zero
+        """
+
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
