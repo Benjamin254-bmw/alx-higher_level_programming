ba2f53a57f11dfada00810d9f624c3c6385f75dc
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-03-29 18:11:55 -0700

    safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    """prints x elements of a list
-    All elements must be printed on the same line followed by a new line.
-    x represents the number of elements to print
-    x can be bigger than the length of my_list
-    returns the real number of elements printed
-    """
-
-
     a = 0
     for i in range(x):
         try:
