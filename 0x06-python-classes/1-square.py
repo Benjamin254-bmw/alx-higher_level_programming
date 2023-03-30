99ccc86622e78aaba1f289beb1860cf0386f71a4
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-03-29 19:01:49 -0700

    defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """create a class named Square
+    """
+
+    def __init__(self, size):
+        """__init__ method initializes the instance
+        arg:
+            size(int): private instance attribute
+        """
+
+        self.__size = 
