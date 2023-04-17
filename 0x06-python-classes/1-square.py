8bd926bb1be42a85fa486dcaae54f93b44b31d4c
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-04-17 01:53:38 -0700

    def square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 class Square:
-    """create a class named Square
-    """
+    """create a class named Square"""
 
     def __init__(self, size):
-        self.__sie = size
-        """__init__ method initializes the instance
-        arg:
-            size(int): private instance attribute
-        """
+        """Initializes the data"""
+        self.__size = size
