df13a4d665d164c49a5951768818b900787ced28
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-04-30 07:42:13 -0700

    real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle:
             ValueError: width must be >= 0"""
         if type(value) is not int:
             raise TypeError('width must be an integer')
-        if value < 0:
+        elif value < 0:
             raise ValueError('width must be >= 0')
         else:
             self.__width = value
@@ -53,7 +53,7 @@ class Rectangle:
         """
         if type(value) is not int:
             raise TypeError('height must be an integer')
-        if value < 0:
+        elif value < 0:
             raise ValueError('height mus be >= 0')
         else:
             self.__height = value
