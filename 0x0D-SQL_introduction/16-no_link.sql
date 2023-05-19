361622b0e0e3ae05861c03393d1b5ceff071061f
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-05-19 17:17:13 +0300

    Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score, *name FROM second_table ORDER BY ASC;
+SELECT score, name FROM second_table WHERE name != '' ORDER BY `score` DESC;
