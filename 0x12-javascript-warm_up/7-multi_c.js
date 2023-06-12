e538cb3e0bf29fce34cb110225e9aa22c96e0daf
Benjamin wachira <wachirabenjamin18@gmail.com>
2023-06-12 20:26:45 +0000

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const num=process.argv[2];
+let num = process.argv[2];
 while(parseInt(num)){
-console.log(num*"C is fun");
+console.log(num * "C is fun");
 }else {
 console.log("Missing number of occurrences");
 }
