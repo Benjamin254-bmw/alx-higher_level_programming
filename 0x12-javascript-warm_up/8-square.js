f572d7afd22a2e297f654da164124f8cc0012eb2
Benjamin wachira <wachirabenjamin18@gmail.com>
2023-06-12 21:01:59 +0000

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let num = parseInt(process.argv[2]);
-if (num === undefined || isNaN(num)){
+if (isNaN(num)){
 console.log("Missing size");
 } else {
 for (i=0; i<num; i++){
@@ -9,6 +9,6 @@ let s='';
 for(x=0; x<num; x++){
 s+="X";
 }
-console.log("X");
+console.log(s);
 }
 }
