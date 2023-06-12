fa206223185bd5075c723c5c8be6098cd5325552
Benjamin wachira <wachirabenjamin18@gmail.com>
2023-06-12 21:51:20 +0000

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,12 +3,11 @@
 function factorials (a){
 if(isNaN(a)){
 return 1;
-}else{
+}
 if(a===1){
 return 1;
 } else {
 return a * factorials(n-1);
-}
 }
 }
 console.log(factorials(parseInt(process.argv[2])));
