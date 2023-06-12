2e4fd69c390571bccea024ffbc78d2ccdfe9ae47
Benjamin wachira <wachirabenjamin18@gmail.com>
2023-06-12 22:13:12 +0000

    Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (process.argv.length<=3){
 console.log('0');
 } else {
-const arry = process.argv.slice(2).map(number);
+const arry = process.argv.slice(2).map(Number);
 const two = arry.sort(function(a, b){return b-a;})[1];
 console.log(two);
 }
