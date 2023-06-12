1b05d15dc2dbe2c8feadd2411047024e0ca91b39
Benjamin wachira <wachirabenjamin18@gmail.com>
2023-06-12 19:04:01 +0000

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length === ''){
+console.log("No argument");
+} else if (process.argv.length === 1){
+console.log("Arguments found");
+} else {
+console.log("Arguments found");
+}
