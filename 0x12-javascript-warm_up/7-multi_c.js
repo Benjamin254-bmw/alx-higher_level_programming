73d287179489607a60b7767352f4af07015f025d
Benjamin wachira <wachirabenjamin18@gmail.com>
2023-06-13 07:46:47 +0000

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ if (num === undefined || isNaN(num)) {
   console.log('Missing number of occurrences');
 } else {
   let i = 0;
-  for (i = 0; i < num; i++){
+  for (i = 0; i < num; i++) {
     console.log('C is fun');
 }
 }
