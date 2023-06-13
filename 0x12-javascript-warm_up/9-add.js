1650f8c3070357d37d8319e13c12cb0203e31415
Benjamin wachira <wachirabenjamin18@gmail.com>
2023-06-13 20:08:09 +0000

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-function add(a, b){
-cinst d=(a + b);
-console.log(d);
+function add(a, b) {
+  const d = (a + b);
+  console.log(d);
 }
 add(Number(process.argv[2]), Number(process.argv[3]));
