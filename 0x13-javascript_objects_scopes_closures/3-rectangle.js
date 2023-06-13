0a6d691e41cd81603facd115fc2e3e8d5ae1aba7
Benjamin wachira <wachirabenjamin18@gmail.com>
2023-06-13 21:50:48 +0000

    Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+class Rectangle {
+  if (w > 0 && h > 0) {
+    constructor (w, h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    let rows, columns = 0;
+    for (rows = 0; rows < this.height; rows++) {
+      let str = '';
+      for (columns = 0; columns < this.width; columns++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+}
+
+module.exports = Rectangle;
