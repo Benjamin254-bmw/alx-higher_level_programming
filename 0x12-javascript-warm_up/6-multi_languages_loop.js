6e7c69481e5c769b76f1b89d5d5407e74e3cce92
Benjamin wachira <wachirabenjamin18@gmail.com>
2023-06-13 07:34:15 +0000

    loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (i=0; i<languages.length; i++){
-console.log(languages[i]);
+let i = 0;
+for (i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
 }
