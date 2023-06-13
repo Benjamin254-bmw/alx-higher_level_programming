8a1b8291702d781a756d208a33f8a84ceed901b6
Benjamin wachira <wachirabenjamin18@gmail.com>
2023-06-13 21:57:31 +0000

    Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 class Rectangle {
-  if (w > 0 && h > 0) {
-    constructor (w, h) {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
