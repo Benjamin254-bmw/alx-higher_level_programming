2fe9a885424edf13f5c029c938ec4fdf56183e3e
Benjamin wachira <wachirabenjamin18@gmail.com>
2023-06-13 07:36:13 +0000

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 let num = parseInt(process.argv[2]);
 
-if (num === undefined || isNaN(num)){
-console.log("Missing number of occurrences");
-}else {
-for (i=0; i<num; i++){
-console.log("C is fun");
+if (num === undefined || isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (i = 0; i < num; i++){
+    console.log('C is fun');
 }
 }
