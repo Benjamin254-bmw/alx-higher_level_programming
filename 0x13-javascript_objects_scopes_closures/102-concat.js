28a83b83e75b784104f875d4a568651291816f42
Benjamin wachira <wachirabenjamin18@gmail.com>
2023-06-16 14:48:27 +0000

    Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const file1 = fs.readFileSync(process.argv[2]);
 const file2 = fs.readFileSync(process.argv[3]);
 const arr = [file1, file2];
-let file = fs.createWriteStream(process.argv[4]);
+const file = fs.createWriteStream(process.argv[4]);
 arr.forEach(function (line) {
   file.write(line);
 });
