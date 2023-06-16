4a91790efd3f302672c1bae750f82d34ab9b3dd4
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-06-16 08:56:17 -0700

    readme

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+script displays states in the database by ascending id order
+the script should take 3 arguements:
+    mysql username, mysql password and database name
+"""
+
+if __name__ = '__main__':
+    import sys
+    import MySQLdb
+
+    if len(sys.argv) > 3:
+        conn = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3],
+                charset="utf8"
+        )
+        curr = conn.cursor()
+        curr.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        curr.close()
+        conn.close()
