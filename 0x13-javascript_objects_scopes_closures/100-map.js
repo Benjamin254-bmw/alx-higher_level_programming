f3ea74384c230330f319a065ce3adecf89d62c2b
Benjamin <77714403+Benjamin254-bmw@users.noreply.github.com>
2023-06-16 17:19:41 +0300

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let list = require('./100-data').list;
-const newList = [];
+const list = require('./100-data').list;
+let newList = [];
 
 newList = list.map(function (n, i) {
   return n * i;
