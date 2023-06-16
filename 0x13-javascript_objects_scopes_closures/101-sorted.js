0f7741684fa506c7ebb4749ef21fab94257cf423
Benjamin wachira <wachirabenjamin18@gmail.com>
2023-06-16 14:33:21 +0000

    Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-let dict = require('./101-data').dict;
-let d = {};
+const dict = require('./101-data').dict;
+const d = {};
 
-for (let l in dict) {
-  if (!(dict[l]) in d) {
-    d[dickt[l]] = [l];
+for (const l in dict) {
+  if (!(dict[l] in d)) {
+    d[dict[l]] = [l];
   } else {
     d[dict[l]].push(l);
   }
