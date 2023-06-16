cb4cbfc86729c1d428a78cc3ae2da3ea9b9d2e84
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-06-16 11:01:29 -0700

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
             charset="utf-8"
     )
     cur = conn.cursor()
-    cur.execute(""SELECT * FROM states WHERE name LIKE BINARY '{}'"".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'".format(sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         if row[1] == sys.argv[4]:
