1bc268bb72862b9a7c449d27c4787f745e153435
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-06-16 11:13:09 -0700

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,19 +6,24 @@ script should take 4 arguments:
     mysql username, mysql password, database name and state name searched
 """
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(
-            host="localhost",
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306
-        )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    query = """SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"""
+    cur.execute(query.format(sys.argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1] == sys.argv[4]:
+            print(row)
     cur.close()
-    db.close()
+    conn.close()
