488a336e2c1e86b8df8d9cfae4e6bb20be39ee7e
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-06-17 12:55:13 -0700

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
+        db=sys.argv[3],
         charset="utf8"
         )
     cur = conn.cursor()
