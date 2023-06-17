8cf2d733db333c5fcfffa49151a00b979278aed4
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-06-17 12:58:49 -0700

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """script that lists all cities from the database hbtn_0e_4_usa
-Your script should take 3 arguments:
+the script should take 3 arguments:
     mysql username, mysql password and database name
 """
 
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         passwd=sys.argv[2],
         db=sys.argv[3],
         charset="utf8"
-        )
+    )
     cur = conn.cursor()
     query = """SELECT cities.id, cities.name, states.name FROM cities INNER JOIN 
     states ON cities.states_id = states.id ORDER BY id ASC"""
