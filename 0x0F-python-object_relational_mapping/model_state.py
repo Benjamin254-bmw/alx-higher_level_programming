61a07e3f5883f83a64aba52d970167a202085c70
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-06-18 12:41:26 -0700

    First state model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""python file that contains the class definition of a State and an instance Base = declarative_base():
+State class:
+    inherits from Base Tips
+    links to the MySQL table states
+    class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
+    class attribute name that represents a column of a string with maximum 128 characters and can’t be null
+"""
+
+from sqlalchemy import Column, Integer, String
+from from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(base):
+    ___tablename__ = 'states'
+    id = Column(Integer, nullable=False, unique=True, primary_key=True)
+    name= Column(String(128), nullable=False)
