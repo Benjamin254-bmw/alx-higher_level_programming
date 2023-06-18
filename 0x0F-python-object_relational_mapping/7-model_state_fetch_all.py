1181c77fda3a20c4a4b115b736666aeaab436e95
Benjamin <77714403+Benjamin254-bmw@users.noreply.github.com>
2023-06-18 23:49:46 +0300

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,13 +5,14 @@
     mysql username, mysql password and database name
 """
 
+if __name__ == "__main__":
+
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
