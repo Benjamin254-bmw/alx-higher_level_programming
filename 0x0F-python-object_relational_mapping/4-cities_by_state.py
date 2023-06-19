e21a399a86af87387a3bbd44d2c9cbe46dc7544d
Benjamin <77714403+Benjamin254-bmw@users.noreply.github.com>
2023-06-19 09:27:40 +0300

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     import sys
     import MySQLdb
 
-    conn=MySQLdb.connect(
+    conn=MySQLdb.connect (
         host="localhost",
         port=3306,
         user=sys.argv[1],
