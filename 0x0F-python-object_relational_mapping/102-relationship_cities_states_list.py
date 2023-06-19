a6024718383441ee41bedbf0360c696c0ea8f9ee
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-06-18 23:56:20 -0700

    From city

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -11,8 +11,9 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import relationship
 
-if __name__ == '__main__':
-     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
