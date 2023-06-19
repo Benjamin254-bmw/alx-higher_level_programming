5ad4586e0d32dbce2121b8d5c73f4f9447990bb6
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-06-18 23:10:45 -0700

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
                 port=3306,
                 user=sys.argv[1],
                 passwd=sys.argv[2],
-                db=sys.argv[3],               
+                db=sys.argv[3],
          )
         curr = db.cursor()
         curr.execute("""SELECT * FROM states ORDER BY states.id ASC""")
