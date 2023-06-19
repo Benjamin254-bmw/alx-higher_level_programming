c5564261053e3ad71d92998a11dfca2a34fc7633
Benjamin <77714403+Benjamin254-bmw@users.noreply.github.com>
2023-06-19 09:33:11 +0300

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """script that lists all State objects from the database hbtn_0e_6_usa
-    Your script should take 3 arguments: 
+    Your script should take 3 arguments:
     mysql username, mysql password and database name
 """
 
