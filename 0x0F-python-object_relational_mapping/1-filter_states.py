5e6454807dc3cfadc018ef2159a65b5ee9bc4a4d
Benjamin <77714403+Benjamin254-bmw@users.noreply.github.com>
2023-06-19 09:17:31 +0300

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:
-    script should take 3 arguments: mysql username, mysql password and database name
+script that lists all states with a name starting with 
+N (upper N) from the database hbtn_0e_0_usa:
+    script should take 3 arguments: 
+        mysql username, mysql password and database name
 """
 
 if __name__ == '__main__':
@@ -18,7 +20,8 @@ if __name__ == '__main__':
                 charset="utf8"
         )
         cur = conn.cursor()
-        cur.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC""")
+        cur.execute("""SELECT * FROM states WHERE name 
+        LIKE 'N%' ORDER BY states.id ASC""")
         query_rows = cur.fetchall()
         for row in query_rows:
             if row[1][0] == 'N':
