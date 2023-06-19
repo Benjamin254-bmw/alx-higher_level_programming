05e290f033f728b1816ea0d3db513ce1f02f7bdf
Benjamin <77714403+Benjamin254-bmw@users.noreply.github.com>
2023-06-19 09:43:01 +0300

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Python file similar to model_state.py named model_city.py that contains
-the class definition of a City.
+"""Python file similar to model_state.py named model_city.py
+that contains the class definition of a City.
     Prints all the City objects from the database
     takes arguments: mysql-username, mysql-password, database-name
 """
