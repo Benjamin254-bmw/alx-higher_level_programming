e8361ff3c623c196d68d98a9df7fcad8e7c087ba
Benjamin <77714403+Benjamin254-bmw@users.noreply.github.com>
2023-06-19 09:25:30 +0300

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument
+"""a script that takes in arguments and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument
     script should take 4 arguments:
         mysql username, mysql password, database name and state name
 """
@@ -10,10 +11,10 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-            host="localhost", 
-            user=sys.argv[1], 
-            passwd=sys.argv[2], 
-            db=sys.argv[3], 
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
             port=3306)
     cur = db.cursor()
     match = sys.argv[4]
