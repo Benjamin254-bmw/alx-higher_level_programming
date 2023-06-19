cd6fea400b2fa650b20653db3e43d71309a4721c
Benjamin <77714403+Benjamin254-bmw@users.noreply.github.com>
2023-06-19 09:37:54 +0300

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa
-    Your script should take 4 arguments: 
+"""script that prints the State object with the name passed as
+argument from the database hbtn_0e_6_usa
+    Your script should take 4 arguments:
     mysql username, mysql password, database name and state name
 """
 
