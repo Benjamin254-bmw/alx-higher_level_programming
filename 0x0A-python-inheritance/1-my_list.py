30e1a5f032fff5a15f6aa3a725183a52e1eeb8d1
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-09-30 12:31:20 +0300

    my list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""contains MyList class
+"""
+
+class MyList(list):
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))
