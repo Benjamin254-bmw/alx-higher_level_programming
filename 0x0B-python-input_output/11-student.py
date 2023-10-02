0e29aab4ac818514d07b687d4681a5a25719881b
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-10-02 19:39:44 +0300

    Student to disk and reload

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -7,7 +7,7 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
-        
+
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
@@ -22,7 +22,7 @@ class Student:
 
         If attrs is a list of strings, represents only those attributes
         included in the list.
-        
+
         Args:
             attrs (list): (Optional) The attributes to represent.
         """
@@ -32,11 +32,10 @@ class Student:
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
-
     def reload_from_json(self, json):
         """Replace all attributes of the Student.
         Args:
             json (dict): The key/value pairs to replace attributes with.
         """
         for k, v in json.items():
-         setattr(self, k, v)
+            setattr(self, k, v)
