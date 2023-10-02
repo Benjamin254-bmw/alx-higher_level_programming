79e20e0af78a021dd192275371b513752b551e5a
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-10-02 18:52:22 +0300

    Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -10,6 +10,6 @@ def append_write(filename="", text=""):
     Returns:
         The number of characters appended.
     """
-    
+
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
