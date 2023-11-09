61145f48110c5281b58b17ef6f8b48c2e5a73e99
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-11-09 04:02:38 +0300

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented!"""
+        raise Exception("area() is not implemented")
