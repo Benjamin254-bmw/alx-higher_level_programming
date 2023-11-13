21b9c2e223a456d1d009d5bb671baf6c92894595
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-11-13 21:25:29 +0300

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""defines a base class model"""
+
+class Base:
+    """base class that represents the base for all other classes.
+    private Class attribute:
+        __nb_object (int): Number of instantiated Bases.
+        """
+    
+    def __init__(self, id=None):
+        """initialize a new base.
+        Args:
+            id (int): identity of the new base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
