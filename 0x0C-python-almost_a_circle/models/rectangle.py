4f6e765d490d57f901624026d7858a34707b7076
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-11-25 15:35:11 +0300

    First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""defines a rectangle class"""
+"""my class module."""
 from models.base import Base
 
 class Rectangle(Base):
-    """represents a rectangle"""
+    """my Rectangle class"""
 
     def __init__(self, width, height, x=0, y=0, id=none):
-        """initialize a new rectangle
+        """class constructor"""
 
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-            x (int): The x coordinate of the new Rectangle.
-            y (int): The y coordinate of the new Rectangle.
-        """
         self.width = width
         self.hight = height
         self.x = x
@@ -22,15 +16,15 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """set or get the width of the rectangle"""
+        """private instance attribute getter"""
         return self.width
 
     @width.setter
     def width(self, value):
         if type(value) != int:
-            raise TypeError("Height must be an integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("Height must be > 0")
+            raise ValueError("width: must be > 0")
         self.__height = value
 
     @property
