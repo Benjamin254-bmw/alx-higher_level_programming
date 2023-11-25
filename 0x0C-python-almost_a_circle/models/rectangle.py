b9c453a1aa6386c4583ef439675f28b3a4c17aea
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-11-25 15:50:59 +0300

    First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@ from models.base import Base
 class Rectangle(Base):
     """my Rectangle class"""
 
-    def __init__(self, width, height, x=0, y=0, id=none):
+    def __init__(self, width, height, x=0, y=0, id=None):
         """class constructor"""
 
         self.width = width
