488323e37f867008760a3a896eabdda924ed4a19
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-11-25 12:58:26 +0300

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,13 +6,14 @@ class Base:
     private Class attribute:
         __nb_object (int): Number of instantiated Bases.
     """
-    
+    __nb_objects = 0
+
     def __init__(self, id=None):
         """initialize a new base.
         Args:
             id (int): identity of the new base.
         """
-        if id is not None:
+        if id:
             self.id = id
         else:
             Base.__nb_objects += 1
