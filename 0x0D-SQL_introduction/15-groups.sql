2dde66e8400c43990c8444f04316fc865ec7bfeb
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-12-13 23:38:48 +0300

    number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score, COUNT(*) FROM second_table AS number GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
