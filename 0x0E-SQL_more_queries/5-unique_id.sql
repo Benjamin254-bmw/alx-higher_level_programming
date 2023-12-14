7deaad557671a222a5c3c42272427098179a89b9
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-12-14 12:54:16 -0800

    unique id

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -2,4 +2,4 @@
 -- unique_id description:
 	-- id INT with the default value 1 and must be unique
 	-- name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS unique_id(id INT(1)UNIQUE , name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE , name VARCHAR(256));
