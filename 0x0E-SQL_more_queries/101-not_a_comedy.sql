00ca21ac85d3c84778196cb668043a5f595bac19
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-12-15 10:50:28 -0800

    no comedy tonight!

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,19 @@
+-- script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+SELECT DISTINCT tv_shows.title
+       FROM tv_show_genres
+       RIGHT JOIN tv_shows
+       ON tv_show_genres.show_id=tv_shows.id
+       LEFT JOIN tv_genres
+       ON tv_show_genres.genre_id=tv_genres.id
+       WHERE tv_shows.title NOT IN
+         (SELECT tv_shows.title
+	   FROM tv_show_genres
+	   LEFT JOIN tv_shows
+	   ON tv_show_genres.show_id=tv_shows.id
+	   LEFT JOIN tv_genres
+	   ON tv_show_genres.genre_id=tv_genres.id
+	   WHERE tv_genres.name='Comedy')
+	ORDER BY tv_shows.title;
