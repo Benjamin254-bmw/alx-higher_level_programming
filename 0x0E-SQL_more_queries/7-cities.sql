12fd9e434db8264896bc7356cb0d03cf61397a8a
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-12-15 07:35:10 -0800

    cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@
 	-- id INT unique, auto generated, can’t be null and is a primary key
 	-- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
 	-- name VARCHAR(256) can’t be null
-CREATE DATABASE FI NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 SUE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(id INT NOT NULL AUTO_INCREMENT, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY(id), FOREIGN KEY REFERENCES stateS(id));
