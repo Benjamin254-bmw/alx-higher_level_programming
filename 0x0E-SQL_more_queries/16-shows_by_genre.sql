93b0d14dd790fe2c37beefdee320441e26bfcc10
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-12-15 10:46:54 -0800

    list shows and genres

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,13 @@
+-- ists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+-- If a show doesn’t have a genre, display NULL in the genre column
+-- Each record should display: tv_shows.title - tv_genres.name
+-- Results must be sorted in ascending order by the show title and genre name
+
+SELECT tv_shows.title, tv_genres.name
+       FROM tv_show_genres
+       RIGHT JOIN tv_shows
+       ON tv_show_genres.show_id=tv_shows.id
+       LEFT JOIN tv_genres
+       ON tv_show_genres.genre_id=tv_genres.id
+       ORDER BY tv_shows.title, tv_genres.name
+       ASC;
