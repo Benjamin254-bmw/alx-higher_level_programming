afbcf4d77e2783c52700858b462bb7a59cf9b9a2
Benjamin254-bmw <wachirabenjamin18@gmail.com>
2024-01-08 15:00:28 +0300

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Script that prints a square
+
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+    console.log("Missing size")
+} else {
+    const l = process.argv[2];
+    let i = 0;
+    
+    while (l > i) {
+        console.log("X".repeat(l));
+        i++;
+    }
+}
