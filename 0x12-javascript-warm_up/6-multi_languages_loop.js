20c310a3b44401c46b1483f3080ee11b3f9f2f4b
Benjamin254-bmw <wachirabenjamin18@gmail.com>
2024-01-08 12:54:44 +0300

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+
+const C = "C is fun\n", Python = "Python is cool\n", JavaScript = "JavaScript is amazing\n"
+const langs = [C, Python, JavaScript]
+
+for (line of langs) {
+    console.log(i)
+}
