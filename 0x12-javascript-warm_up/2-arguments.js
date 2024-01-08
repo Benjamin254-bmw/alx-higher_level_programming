d2af4608e584524030cff53d6c24687fdeb99931
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-08 20:34:36 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 // Script that prints a message depending of the number of arguments passed:
-
 if (process.argv.length === 2){
     console.log('No argument');
 } else if (process.argv.lenght === 3){
