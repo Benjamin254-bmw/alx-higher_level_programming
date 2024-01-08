b6c44b9c26839eb43b31f564c0ace79fdcb59180
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-08 20:33:18 +0300

    Arguments

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
-// Write a script that prints 3 lines:
-const C = "C is fun\n"
-const Python = "Python is cool\n"
-const JavaScript = "JavaScript is amazing\n"
-console.log(C, Python, JavaScript)
+const C = 'C is fun'\n;
+const Python = 'Python is cool'\n;
+const JavaScript = 'JavaScript is amazing'\n;
+console.log(C, Python, JavaScript);
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 // Script that prints a message depending of the number of arguments passed:
 
 if (process.argv.length === 2){
-    console.log("No argument");
+    console.log('No argument');
 } else if (process.argv.lenght === 3){
-    console.log("Argument found");
+    console.log('Argument found');
 } else {
-    console.log("Arguments found");
+    console.log('Arguments found');
 }
