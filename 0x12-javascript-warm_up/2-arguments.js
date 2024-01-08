cf882840c3e3f0ccf2dfb08ebe72a681141fea1d
Benjamin254-bmw <wachirabenjamin18@gmail.com>
2024-01-08 12:18:12 +0300

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Script that prints a message depending of the number of arguments passed:
+
+if (process.argv.length === 2){
+    console.log("No argument");
+} else if (process.argv.lenght === 3){
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
