a534d97c44107875c429fb035d7b8631fa8483bd
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-09 12:05:48 +0300

    Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Script that prints the first argument passed to it:
-if (process.argv[2] === undefined){
-    console.log('No argument');
+if (process.argv[2] === undefined) {
+  console.log('No argument');
 } else {
-console.log(process.argv[2]);
+  console.log(process.argv[2]);
 }
