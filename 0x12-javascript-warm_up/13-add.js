9edae06d6723571fdf917276ce22716cd85848b2
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-09 13:00:07 +0300

    Add file

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // A function that returns the addition of 2 integers.
 
-exports.add = function add(a, b) {
-    return(a + b);
+exports.add = function add (a, b) {
+  return (a + b);
 };
