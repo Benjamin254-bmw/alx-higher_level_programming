5768d47dbfeb88bba1843fda317968408134b765
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-09 11:54:14 +0300

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-cosole.log(' C is fun\n');
-console.log('Python is cool\n');
-console.log('JavaScript is amazing\n');
+cosole.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
