d5c9a9feec59e051ec3e6bc2a019a647cd5dbc8e
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-09 18:59:45 +0300

    Square #0

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const Rectatngle = require('./4-rectangle');
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    suer(size, size);
+    super(size, size);
   }
 }
 module.exports = Square;
