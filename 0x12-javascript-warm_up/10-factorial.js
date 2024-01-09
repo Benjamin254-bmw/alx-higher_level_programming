2af00baf87e937c681bc7e6f2d5610387c97162c
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-09 12:41:24 +0300

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 // Script that computes and prints a factorial
 
-const a = (process.argv[2])
+const a = (process.argv[2]);
 
-function factorial(n){
-    if (n < 0){
-        return (-1);
-    }
-    if (n === 0 || isNaN(n)) {
-        return (1);
-    }
-    return (n * factorial(n-1));
+function factorial(n) {
+  if (n < 0) {
+    return (-1);
+  }
+  if (n === 0 || isNaN(n)) {
+    return (1);
+  }
+  return (n * factorial(n-1));
 }
 
 console.log(factorial(Number(a)));
