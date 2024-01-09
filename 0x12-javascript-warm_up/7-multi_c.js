23661acce0f4027d2dee0c0d2297c001c55bf680
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-09 12:23:08 +0300

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,11 +2,11 @@
 // Script that prints x times "C is fun"
 
 if (isNaN(process.argv[2] || process.argv[2] === undefined)) {
-    console.log("Missing number of occurences")
+  console.log('Missing number of occurences');
 } else {
-    let i = 0;
-    while (Number(process.argv[2]) > i){
-        console.log("C is fun");
-        i++;
-    }
+  let i = 0;
+  while (Number(process.argv[2]) > i) {
+    console.log('C is fun');
+    i++;
+  }
 }
