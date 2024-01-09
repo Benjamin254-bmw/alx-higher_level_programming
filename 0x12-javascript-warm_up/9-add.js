00f53a7ef9f773827a097de74cc484f4c99481f4
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-09 12:30:47 +0300

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // Script that prints the addition of 2 integers
 
-const a = process.argv[2]
-const b = process.argv[3]
+const a = process.argv[2];
+const b = process.argv[3];
 
-function add(a, b){
-    console.log(a + b);
+function add( a, b) {
+  console.log(a + b);
 }
 add(Number(a), Number(b));
