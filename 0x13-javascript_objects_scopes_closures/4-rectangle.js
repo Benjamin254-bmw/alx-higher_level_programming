f8861b93f8677efa3418dc8beb8ced8cd623e402
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-09 18:19:51 +0300

    Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (w > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let t = 0; t < this.width; t++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
+
+  rotate () {
+    const rot = this.width;
+    this.width = this.height;
+    this.height = rot;
+  }
+  doube () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+  
+  module.exports = Rectangle;
