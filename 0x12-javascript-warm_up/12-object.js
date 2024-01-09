091102062c8d113f161e92199aefec5826fcf477
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-09 12:58:18 +0300

    Object

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -2,9 +2,9 @@
 // Script to replace the value 12 with 89:
 
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  myObject.value = 89;
-  console.log(myObject);
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
