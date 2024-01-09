e0be9008c6b076e2f29ee2d92ab5791dbaffeaba
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-09 13:18:57 +0300

    Increment object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,18 +2,18 @@
 // Function incr that increments the integer value.
 
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
   
-  myObject.value = function () {
-    this.value++;
-  }
+myObject.incr = function () {
+  this.value++;
+}
   
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
