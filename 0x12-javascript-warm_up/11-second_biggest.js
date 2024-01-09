e467e44b1f9a08634caa209691905bd80acafc08
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-09 12:56:05 +0300

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 // Script that searches the second biggest integer in the list of arguments
-
-if (process.argv.length <= 3 {
+if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const arr = process.argv.slice(2).map(Number);
