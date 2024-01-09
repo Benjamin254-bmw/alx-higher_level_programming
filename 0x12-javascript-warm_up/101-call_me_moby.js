084355ef8cb27b45e93d92b200eca06bbbb0815f
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-09 13:01:46 +0300

    Call me Moby

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -2,7 +2,7 @@
 // A function that executes x times a function
 
 exports.callMeMoby = function (x, theFunction) {
-    for (let i = 0; i < x; i++) {
-        theFunction();
-    }
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
 }
