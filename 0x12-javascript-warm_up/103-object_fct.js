4b271987bd3c579667585d4706a4eb881d3cd758
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-09 13:21:06 +0300

    Increment object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -6,11 +6,11 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-  
+
 myObject.incr = function () {
   this.value++;
-}
-  
+};
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
