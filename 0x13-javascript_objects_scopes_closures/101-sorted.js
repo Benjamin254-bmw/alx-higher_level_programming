b76ddd628274b6bfebe78eb9685eb7c72783bfd3
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-11 09:51:31 +0300

    Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -6,12 +6,12 @@ const vals = Object.values(dict);
 const valsUniq = [...new Set(vals)];
 const newDict = {};
 for (const j in valsUniq) {
-	  const list = [];
-	  for (const k in totalist) {
-		      if (totalist[k][1] === valsUniq[j]) {
-			            list.unshift(totalist[k][0]);
-			          }
-		    }
-	  newDict[valsUniq[j]] = list;
+  const list = [];
+  for (const k in totalist) {
+    if (totalist[k][1] === valsUniq[j]) {
+      list.unshift(totalist[k][0]);
+    }
+  }
+  newDict[valsUniq[j]] = list;
 }
 console.log(newDict);
