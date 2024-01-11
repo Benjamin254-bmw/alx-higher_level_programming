99cdd5320b35e93976410754e7a96fcf2e682245
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-11 09:45:58 +0300

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,15 +3,15 @@ const SquareP = require('./5-square');
 
 class Square extends SquareP {
   charPrint (c) {
-  if (c === undefined) {
-    c = 'X';
-  }
-  for (let i = 0; i < this.height; i++) {
-    let s = '';
-    for (let j = 0; j < this.width; j++) {
-      s += c;
+    if (c === undefined) {
+      c = 'X';
     }
-    console.log(s);
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
     }
   }
 }
