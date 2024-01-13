a3cad9bb3086073bd054938a9af9b7323123eb28
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-13 20:21:04 +0300

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -11,7 +11,7 @@ if __name__=="__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = sessio.query(State).filter(State.name == (sys.argv[4],))
+    instance = session.query(State).filter(State.name == (sys.argv[4],))
     try:
         print(instance[0].id)
     except IndexError:
