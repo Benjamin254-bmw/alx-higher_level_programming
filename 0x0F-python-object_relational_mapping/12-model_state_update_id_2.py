828585b78d8c2274ae249bba42eb8a3fa6eb7593
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-13 21:05:30 +0300

    Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -9,6 +9,7 @@ if __name__=="__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
+    session = Session()
     new_instance = sessiom.query(State).filter_by(id=2).first()
     new_instance.name = 'New Mexico'
     session.commit()
