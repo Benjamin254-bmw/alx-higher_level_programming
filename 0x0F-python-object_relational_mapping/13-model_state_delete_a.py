a23ed8072046e200e45c07fb6ebba794d89ec68a
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-13 21:11:50 +0300

    Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -3,7 +3,7 @@
 the letter a from the database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import (create_engine)
-from sqlalchemy import sessionmaker
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__=="__main__":
