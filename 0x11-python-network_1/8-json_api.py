2a6a3f979ec5d4e03840261b1e7d7d4adabadd90
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-27 01:44:50 +0300

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,9 +10,10 @@ import requests
 if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+
+    req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = r.json()
+        response = req.json()
         if response == {}:
             print("No result")
         else:
