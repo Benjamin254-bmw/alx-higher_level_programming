754a6aa1cece68dff246d6be5c1e5924da67d594
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-27 01:31:32 +0300

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
     r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
         response = r.json()
-    if response == {}:
-        print("No result")
-    else:
-        print("[{}] {}".format(response.get("id"), response.get("name")))
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
         print("Not a valid JSON")
