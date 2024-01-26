f6249600be42940ddd25302187445446a4788bf5
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-27 01:27:25 +0300

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
             sys.argv[2], sys.argv[1])
 
-
     req = requests.get(url)
     commits = req.json()
     try:
