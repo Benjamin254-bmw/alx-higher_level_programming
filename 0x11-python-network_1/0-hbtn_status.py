91a145df401bbaf3323cee0ea3611c6eec332ffe
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-27 00:11:24 +0300

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Fetches https://alx-intranet.hbtn.io/status"""
 
-import urllib.request
-
 if__name__=="__main__":
+    import urllib.request
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
