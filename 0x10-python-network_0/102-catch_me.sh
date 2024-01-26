506356c9d5352b0df1f8f698176bf1a0d120ea9a
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-26 23:52:13 +0300

    Catch me if you can!

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Makes a request to 0.0.0.0:5000/catch_me : server responds with message 'You got me!'
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
