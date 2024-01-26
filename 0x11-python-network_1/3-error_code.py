b1038e95512238910daf066b5ef8f07488cd775c
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-27 00:40:36 +0300

    Erro code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,13 @@
 #!usr/bin/python3
-"""sends a request to the URL and
+"""Script that:
+-takes URL and sends a request to the URL
 -displays the body of the response ( utf-8).
 """
 
 if __name__ == "__main__":
     import sys
     from urllib import request, error
-    
+
     try:
         with request.urlopen(sys.argv[1]) as res:
             print(res.read().decode('UTF-8'))
