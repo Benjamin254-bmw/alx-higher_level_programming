69cb82417616c4bc1bdc434cda389fd1d6f40556
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-27 00:51:42 +0300

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 """fetches https://intranet.hbtn.io/status."""
- import requests
+import requests
 
- if __name__ == "__main__":
-     r = requests.get("https://intranet.hbtn.io/status")
-     print("Body response:")
-     print("\t- type: {}".format(type(r.text)))
-     print("\t- content: {}".format(r.text))
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
