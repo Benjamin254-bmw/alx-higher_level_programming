f94c68cdb47166ebb1a461cd09e6f3537bcacdcd
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-01-27 01:26:45 +0300

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,12 +6,15 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            sys.argv[2], sys.argv[1])
+
+
     req = requests.get(url)
     commits = req.json()
     try:
         for i in range(10):
             print("{}: {}".format(commits[i].get("sha"), 
                 commits[i].get("commit").get("author").get("name")))
-            except IndexError:
-                pass
+    except IndexError:
+        pass
