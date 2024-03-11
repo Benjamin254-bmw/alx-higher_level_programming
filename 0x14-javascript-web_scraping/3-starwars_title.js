c32581ad7a3f75a7b84e9e0dfbbaf10e9b8031ee
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-03-12 00:08:41 +0300

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -10,16 +10,4 @@ request(url, function (error, response, body) {
   const data = JSON.parse(body);
 
   console.log(data.title);
-});#!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  const data = JSON.parse(body);
-
-  console.log(data.title);
 });
